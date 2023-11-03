a3e8c5d9cfb406cb5b0904225d392226252ab7eb
Yann Chauvel <yannchvl@gmail.com>
2023-11-03 09:09:07 +0100

    --wip-- [skip ci]

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	d "generate-raffle-book/document"
@@ -12,12 +13,16 @@ var (
 	start *int
 	count *int
 	help  *bool
+	temp  *string
+	force *bool
 )
 
 func init() {
 	start = flag.Int("start", 1, "what is the 1st desired number ?")
-	count = flag.Int("count", 10, "how many books would you like ?")
+	count = flag.Int("count", 10, "how many raffle tickets would you like ?")
 	help = flag.Bool("help", false, "show this help")
+	temp = flag.String("template", "", "path to the customs file to create")
+	force = flag.Bool("force", false, "enforce overwriting customs file")
 }
 
 func main() {
@@ -25,6 +30,11 @@ func main() {
 
 	if *help {
 		DisplayHelp()
+		os.Exit(0)
+	}
+
+	if *temp != "" {
+		OutputCustomFile()
 		os.Exit(0)
 	}
 
@@ -52,7 +62,7 @@ func LoadParameters() (p.Parameters, error) {
 	if err != nil {
 		return parameters, err
 	}
-	customs, err := p.LoadCustoms("assets/customs.json")
+	customs, err := p.LoadCustoms("./customs.json")
 	if err != nil {
 		return parameters, err
 	}
@@ -65,6 +75,26 @@ func LoadParameters() (p.Parameters, error) {
 func DisplayHelp() {
 	fmt.Println("Usage: generate-raffle-book [options]")
 	fmt.Println("Options:")
-	fmt.Println("  -start=1\n What is the 1st desired start ticket number ?")
-	fmt.Println("  -count=10\n How many tickets would you like ?")
+	flag.PrintDefaults()
+}
+
+func OutputCustomFile() {
+	if !*force {
+		stat, _ := os.Stat("customs.json")
+		if stat != nil {
+			fmt.Println("customs.json file already exists. Rerun the command with -force flag to enforce overwrtiting it.")
+			os.Exit(1)
+		}
+	}
+	output, err := json.MarshalIndent(p.CreateBoilerPlateCustoms(), "", "  ")
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	err = os.WriteFile("customs.json", output, 0644)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	fmt.Println("customs.json file created")
 }
